db353d4e562b9543169ded790dc13859db303455
Joey Bright <hello@joeybright.com>
2016-04-18 15:25:06 -0700

    Better build/production options for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 
-var webpack = require('webpack')
-    extractTextPlugin = require('extract-text-webpack-plugin')
-const webpack = require('webpack');
-const extractTextPlugin = require('extract-text-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+var extractTextPlugin = require('extract-text-webpack-plugin');
+var CleanPlugin = require('clean-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+const BUILD_DIR = './build';
 
 // webpack.config.js
-module.exports = {
+const common = {
 
     entry: './entry.jsx',
 
     output: {
         filename: 'bundle.js',
-        path: './',
-        libraryTarget: 'umd'
+        path: BUILD_DIR
     },
 
     module: {
@@ -37,10 +38,68 @@ module.exports = {
     },
 
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({minimize: true}),
         new extractTextPlugin('style.css', {
             allChunks: true
         })
     ]
 
-}
+};
+
+// Dev specific webpack configurations
+if (TARGET === 'start' || !TARGET) {
+
+    module.exports = merge(common, {
+
+        devServer: {
+
+            // Config options specific to dev. environment
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+
+            host: process.env.HOST,
+            port: 3000,
+
+            stats: 'errors-only'
+
+        },
+
+        devTool: 'eval-source-map',
+
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+
+    });
+
+};
+
+// Production specific webpack configuration
+if (TARGET === 'build') {
+
+    module.exports = merge(common, {
+
+        plugins: [
+            // Cleans the exisitin build directory (if there is one)
+            new CleanPlugin(BUILD_DIR + "/*"),
+            // Lets React know this is a production build
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV' : '"production"'
+            }),
+            // Minifies the javascript
+            new webpack.optimize.UglifyJsPlugin({
+                minimize: true,
+                // With comments stripped out
+                output: {
+                    comments: false
+                },
+                compress: {
+                    warnings: false
+                }
+            })
+        ]
+
+    });
+
+};
